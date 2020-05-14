1f8ddfee6ded2094822284f86496e5969400f79f
ntdim <ntdim1973@gmail.com>
2020-05-14 23:13:34 +0500

    extract id parser file

--- src/ru/ntdim/Main.java
+++ src/ru/ntdim/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,15 +16,15 @@ public class Main {
             System.out.printf("Не указан входящий файл!");
             return;
         }
-        String fileIn = args[1];
+        String fileIn = args[0];
         try (Stream<String> stream = Files.lines(Paths.get(fileIn))) {
-            stream
+            List<String> outList = stream
                     .map(s -> s.substring(1, s.indexOf(";")))
                     .map(s -> s.replaceAll("\"", ""))
                     .collect(Collectors.toList());
 
             try (PrintStream out = new PrintStream(new FileOutputStream("skip.txt"))) {
-                out.print(stream);
+                out.print(outList);
             }
         } catch (IOException e) {
             e.printStackTrace();
